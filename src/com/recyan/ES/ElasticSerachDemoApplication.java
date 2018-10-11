1b832450ac38f7107414badc51a5a1da60ad7fc6
Yan <1757901534@qq.com>
2018-10-11 21:35:11 +0800

    启动类

---
+++ src/com/recyan/ES/ElasticSerachDemoApplication.java
@@ -0,0 +1,12 @@
+package com.recyan.ES;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticSerachDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticSerachDemoApplication.class, args);
+	}
+}
