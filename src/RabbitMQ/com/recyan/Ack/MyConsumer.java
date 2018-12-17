725f112b02dc469412f959e4169a13c095ec700f
Yan <1757901534@qq.com>
2018-12-17 23:00:40 +0800

    消费端ACK  与 重回队列

---
+++ src/RabbitMQ/com/recyan/Ack/MyConsumer.java
@@ -0,0 +1,43 @@
+package com.recyan.Ack;
+
+import java.io.IOException;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+
+/**
+ * 测试 消费端手动ACk  &&&&必须要关闭 autoAck = false&&&&
+ * 		消息重回队列
+ */
+public class MyConsumer extends DefaultConsumer {
+
+
+	private Channel channel ;
+	
+	public MyConsumer(Channel channel) {
+		super(channel);
+		this.channel = channel;
+	}
+
+	@Override
+	public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+		System.err.println("-----------consume message----------");
+		System.err.println("body: " + new String(body));
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		if((Integer)properties.getHeaders().get("num") == 0) {
+			// requeue = true 则未消息会重新回到队列中
+			channel.basicNack(envelope.getDeliveryTag(), false, true);
+		} else {
+			channel.basicAck(envelope.getDeliveryTag(), false);
+		}
+		
+	}
+
+
+}
