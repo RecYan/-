3cdefc66b6aa76d18b1a9707889aeaaa3b1b1b4e
Yan <1757901534@qq.com>
2018-12-24 16:24:31 +0800

    集成SpringBoot配置

---
+++ src/RabbitMQ/com/recyan/SpringBootProvider/main/java/com/recyan/springboot/MainConfig.java
@@ -0,0 +1,10 @@
+package com.recyan.springboot;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan({"com.recyan.springboot.*"})
+public class MainConfig {
+
+}
